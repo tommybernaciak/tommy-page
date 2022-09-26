e4987a2cab2e6d70f3c0c437a7aca2abc813ebd4
Tommy Bernaciak <contact@tommybernaciak.com>
2022-09-26 10:53:47 +0200

    added more blog posts

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -11,6 +11,11 @@ export const blog: IBlogPost[] = [
 		href: 'https://tommybernaciak.hashnode.dev/usetransition-react-hook'
 	},
 	{
+		title: 'A missing Jest mock feature',
+		date: 'Mar 10, 2020',
+		href: 'https://medium.com/@tommybernaciak/a-missing-jest-mock-feature-b89b74d7a09c'
+	},
+	{
 		title: 'Redux Saga Pattern for React',
 		date: 'Feb 3, 2020',
 		href: 'https://tommybernaciak.hashnode.dev/redux-saga-pattern-for-react'
@@ -19,5 +24,25 @@ export const blog: IBlogPost[] = [
 		title: 'Clean up request in the useEffect React Hook',
 		date: 'Aug 28, 2019',
 		href: 'https://tommybernaciak.hashnode.dev/clean-up-request-in-the-useeffect-react-hook'
+	},
+	{
+		title: 'Image Optimization — Lazy Loading for Angular using lazysizes',
+		date: 'Aug 22, 2018',
+		href: 'https://medium.com/@tommybernaciak/image-optimization-lazy-loading-for-angular-using-lazysizes-1b8a1fb8e1f8'
+	},
+	{
+		title: 'Subscribe your users to Mailchimp with Gibbon',
+		date: 'Mar 22, 2018',
+		href: 'https://medium.com/@tommybernaciak/subscribe-your-users-to-mailchimp-with-gibbon-1ffa8c19b4e5'
+	},
+	{
+		title: 'React and Flux for Beginners',
+		date: 'Feb 23, 2018',
+		href: 'https://medium.com/@tommybernaciak/react-and-flux-for-beginners-821ef3d4d9fd'
+	},
+	{
+		title: 'Angular on Rails. How to start?',
+		date: 'Feb 6, 2018',
+		href: 'https://medium.com/@tommybernaciak/angular-on-rails-how-to-start-37e3851ffb60'
 	}
 ];
